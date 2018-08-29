8f7fb492db91e2c6373844b9ca018a6ee4405d41
dongdong <286084845@qq.com>
2018-08-29 22:38:41 +0800

    键盘导航2.0

--- main.js
+++ main.js
@@ -79,33 +79,39 @@ var keyboard = {
 
 // 保存按键对应的网址
 var hash = {
-    'q': 'www.qq.com/',
-    'w': 'www.chinaw3c.org/',
-    'e': 'www.ele.me/home/',
-    'r': '',
-    't': 'www.taptap.com/',
-    'y': 'www.youku.com/',
-    'u': 'www.uc.cn/',
-    'i': 'www.iqiyi.com/',
-    'o': '',
-    'p': '',
-    'a': 'www.alibaba.com/',
-    's': '',
-    'd': '',
-    'f': '',
-    'g': 'www.google.com.hk/',
-    'h': '',
-    'j': '',
-    'k': '',
-    'l': '',
-    'z': 'www.zhihu.com/',
-    'x': '',
-    'c': '',
-    'v': '',
-    'b': 'www.bilibili.com/',
-    'n': '',
-    'm': 'www.imooc.com/'
+    'Q': 'www.qq.com/',
+    'W': 'weibo.com/',
+    'E': 'mail.163.com/',
+    'R': 'www.renren.com/',
+    'T': 'www.taptap.com/',
+    'Y': 'www.yahoo.com/',
+    'U': 'www.uc.cn/',
+    'I': 'www.iqiyi.com/',
+    'O': 'www.oppo.com/cn/',
+    'P': 'www.apple.com/',
+    'A': 'www.alibaba.com/',
+    'S': 'www.suning.com/',
+    'D': 'book.dangdang.com/',
+    'F': 'www.ifeng.com/',
+    'G': 'github.com/',
+    'H': 'www.firefox.com.cn/',
+    'J': 'www.jd.com/',
+    'K': 'www.kugou.com/',
+    'L': 'lol.qq.com/',
+    'Z': 'www.zhihu.com/',
+    'X': 'www.panda.tv/',
+    'C': 'cf.qq.com/',
+    'V': 'code.visualstudio.com/',
+    'B': 'www.bilibili.com/',
+    'N': 'www.nipic.com/',
+    'M': 'www.imooc.com/'
 };
+
+// 取出localStorage中的zzz对应的hash，并存到hash中
+var hashInlocalStorage = JSON.parse(localStorage.getItem('zzz') || 'null');
+if (hashInlocalStorage) {
+    hash = hashInlocalStorage;
+}
 
 // 将所有按键数据放到一个新的数组中
 var newArr = [];
@@ -124,7 +130,6 @@ for (var key in keyboard) {
 // 生成键盘
 var ol = document.createElement('ol');  // 生成ol
 ol.className = 'keyboard';
-
 
 for (var i = 0; i < 64; i ++) {
     var li = document.createElement('li');  // 生成64个li
@@ -161,16 +166,73 @@ for (var i = 0; i < 64; i ++) {
     if (i === 62) {
         li.className = 'arrow down';
     }
+
     var span = document.createElement('span');  // 生成span
-    var kbd = document.createElement('kbd');
+    var kbd = document.createElement('kbd');    // 生成kbd
+
+    // 创建img
+    var img = document.createElement('img');
+    
     span.textContent = newArr[i]['text'];
     if (newArr[i]['text2']) {
         kbd.textContent += newArr[i]['text2'];
     }
+
     li.appendChild(span);
     li.appendChild(kbd);
+
+    // 把字母按键的kbd替换成img
+    if (i >= 15 && i <= 24 || i >= 29 && i <= 37 || i >= 42 && i <= 48) {
+        
+        li.replaceChild(img, kbd);
+        var txt = img.previousElementSibling.textContent;
+        if (hash[txt]) {
+            img.src = 'http://' + hash[txt] + 'favicon.ico';
+        } else {
+            img.src = '//i.loli.net/2018/08/29/5b863395bca68.png';
+        }
+        img.onerror = function (event) {
+            event.target.src = '//i.loli.net/2018/08/29/5b863395bca68.png';
+        };
+
+
+        var button = document.createElement('button');
+        button.textContent = '编辑';
+        button.id = li.firstElementChild.textContent;
+        button.onclick = function (event) {
+
+            
+
+            var button2 = event['target'];
+            var img2 = button2.previousSibling;
+            var key = button2['id'];
+            var webSite = prompt('请输入网址');
+            hash[key] = webSite;    // hash 变更
+            
+            img2.src = 'http://' + webSite + '/favicon.ico';
+            img2.onerror = function (event) {
+                event.target.src = '//i.loli.net/2018/08/29/5b863395bca68.png';
+                
+            };
+            localStorage.setItem('zzz', JSON.stringify(hash)); 
+
+            
+            
+
+        // img2.src = 'http://' + webSite + '/favicon.ico';
+        };
+        li.appendChild(button);
+        
+
+        
+        // img.src = 'http://' + hash[txt] + 'favicon.ico';
+        
+    }
+    
     
     ol.appendChild(li);
+
+    
 
 }
 
@@ -178,8 +240,12 @@ for (var i = 0; i < 64; i ++) {
 
 
 
-
-
-
-
 main.appendChild(ol);
+
+document.onkeypress = function (event) {
+    // hash[event['key']] 按键对应的网址
+    console.log(event);
+    var key = event['key'];
+    key = key.toUpperCase();
+    window.open('http://' + hash[key], '_blank');
+};
